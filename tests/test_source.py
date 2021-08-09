9188ff9e50824d76fde721960efd320b49135537
Cian <cian.omondi@student.moringaschool.com>
2021-08-09 22:57:26 +0300

    tests added

--- tests/test_source.py
+++ tests/test_source.py
@@ -14,7 +14,7 @@ class testSource(unittest.TestCase):
         Initiallizing classes from both modules
         '''
 
-        self.news_source = news_source("ABC-News","abc-news",'https://image.tmdb.org/t/p/w500/khsjha27hbs',"latest news on abc","https://www.w3schools.com/html/default.asp","2021-12-03")
+        self.news_source = news_source("ABC-News","abc-news","John",'https://image.tmdb.org/t/p/w500/khsjha27hbs',"latest news on abc","https://www.w3schools.com/html/default.asp","2021-12-03")
         self.news_article = new_article("Cian","News article is great","english","business","https://www.w3schools.com/html/default.asp","US")
 
     
